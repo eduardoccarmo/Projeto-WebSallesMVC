6e99f7338a9c2467474a661f9c39b40797f97595
eduardoccarmo <eduardo.ccarmo@outlook.com>
2022-12-05 22:31:16 -0300

    254 - Primeiro teste MVC: Departamento

---
+++ SallesWebMvc/Controllers/DepartamentoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.AspNetCore.Mvc;
+using SallesWebMvc.Models;
+using System.Collections.Generic;
+
+namespace SallesWebMvc.Controllers
+{
+    public class DepartamentoController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departamento> departamentos = new List<Departamento>();
+
+            departamentos.Add(new Departamento { Id = 1, Nome = "Eletrônicos" });
+            departamentos.Add(new Departamento { Id = 2, Nome = "Vendas" });
+
+            return View(departamentos);
+        }
+    }
+}
